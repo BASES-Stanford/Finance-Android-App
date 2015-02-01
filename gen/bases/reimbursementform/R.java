220da99ec0519f5f1d6a211953cbbeb19cb27fbb
Tony Tan-Torres <tony@gargoyle.co>
2015-01-31 19:28:12 -0800

    Added take photo of receipt button and edit receipt name field. Still need to implement functionality of buttons

--- gen/bases/reimbursementform/R.java
+++ gen/bases/reimbursementform/R.java
@@ -1640,7 +1640,6 @@ containing a value of this type.
         public static final int logo=0x7f020034;
     }
     public static final class id {
-        public static final int ImageView01=0x7f090047;
         public static final int Spinner01=0x7f090043;
         public static final int action_bar=0x7f090031;
         public static final int action_bar_activity_content=0x7f090003;
@@ -1655,12 +1654,12 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f09004a;
+        public static final int action_settings=0x7f09004c;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
-        public static final int button1=0x7f090049;
-        public static final int button2=0x7f090048;
+        public static final int button1=0x7f090047;
+        public static final int button2=0x7f09004b;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f09002f;
@@ -1674,6 +1673,7 @@ containing a value of this type.
         public static final int editText4=0x7f090042;
         public static final int edit_message=0x7f090040;
         public static final int edit_query=0x7f090033;
+        public static final int edit_receipt_name=0x7f090048;
         public static final int end=0x7f09001c;
         public static final int expand_activities_button=0x7f090022;
         public static final int expanded_menu=0x7f090028;
@@ -1710,6 +1710,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f090004;
         public static final int submit_area=0x7f09003c;
         public static final int tabMode=0x7f09000c;
+        public static final int takePhoto=0x7f09004a;
+        public static final int textView1=0x7f090049;
         public static final int title=0x7f090027;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
